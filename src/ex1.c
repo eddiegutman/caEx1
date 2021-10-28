991fff06b29528aa85a7237377580bdfa7bf9af0
Eddie Gutman <edigutman@gmail.com>
2021-10-28 20:13:18 +0300

    Finished the second task

--- src/ex1.c
+++ src/ex1.c
@@ -1,7 +1,12 @@
 #include "ex1.h"
 
 /**
- * Checks the current machine endianness
+ * Checks the current machine endianness.
+ *
+ * We assign the value '1' to num, which means the LSB of num equals to '1' and all the other bytes
+ * are '0'.
+ * Then we check the first byte in memory of num: if its '1' then our LSB is first in memory therefore
+ * its little endian machine, and if its '0' then its a big endian machine.
  *
  * @return 1 for big endian and 0 for little endian.
  */
@@ -19,11 +24,43 @@ int is_big_endian() {
 /**
  * Merges the first half bytes (from the LSB) from y and the remaining bytes from x.
  *
+ * first check for the machine endian, and based on it decide the copying order.
+ * if its a big endian the MSB is first in the memory therefore 'x' need to be copied first.
+ * if its a little endian the LSB is first in the memory therefore 'y' need to be copied first.
+ * after that we iterate on the memory of 'merged' and copy the desired byte from 'x' and 'y',
+ * respectively to the order we decided.
+ *
  * @param x first given word.
  * @param y second given word.
  * @return new merged word.
  */
 unsigned long merge_bytes(unsigned long x, unsigned long int y) {
+    unsigned long merged;
+    int length = sizeof(long);
+
+    // define byte size pointer for each word.
+    char* byte = (char *) &merged;
+    char* first;
+    char* second;
+
+    // decide the copy order based on the machine endian.
+    if (is_big_endian()) {
+        first = (char *) &x;
+        second = (char *) &y;
+    } else {
+        first = (char *) &y;
+        second = (char *) &x;
+    }
+
+    // copy the desired bytes from 'x' and 'y' to merged.
+    for (int i = 0; i < length; i++) {
+        if (i < length/2)
+            *(byte + i) = *(first + i);
+        else
+            *(byte + i) = *(second + i);
+    }
+
+    return merged;
 }
 unsigned long put_byte(unsigned long x, unsigned char b, int i){
 
